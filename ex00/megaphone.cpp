c498514fe4f6d37ce46a2c9ef16b3b16457e62e3
Clément Hamada <clementhamada@protonmail.com>
2020-04-25 17:57:58 +0200

    ex00: Convert indentation to tabs

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,20 +3,20 @@
 
 std::string strupper(const char *in)
 {
-    std::string out = in;
+	std::string out = in;
 
-    for (char &c : out)
-        c = toupper(c);
-    return (out);
+	for (char &c : out)
+		c = toupper(c);
+	return (out);
 }
 
 int main(int ac, const char **av)
 {
-    if (ac == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    else
-        for (int i = 1; i < ac; i++)
-            std::cout << strupper(av[i]);
-    std::cout << "\n";
-    return (0);
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+		for (int i = 1; i < ac; i++)
+			std::cout << strupper(av[i]);
+	std::cout << "\n";
+	return (0);
 }
